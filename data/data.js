c69ce92c33af2537f1344a0e58aa8f98a77e9e81
arodriguez <asier.rodriguez@onenpro.com>
2024-07-21 21:02:50 +0200

    add 2024 till tour + 2007

--- data/data.js
+++ data/data.js
@@ -1,28 +1,28 @@
 var medalleroHistorico = [
     {
         "posicion": 1,"nombre": "asiermaster",
-        "oro": 102,"plata": 56, "bronce": 39,"npod": 114
+        "oro": 103,"plata": 56, "bronce": 39,"npod": 115
     },{
         "posicion": 2, "nombre": "gurrutxaga",
-        "oro": 59, "plata": 50, "bronce": 32, "npod": 158
+        "oro": 59, "plata": 50, "bronce": 33, "npod": 158
     },{
         "posicion": 3, "nombre": "OIER19",
-        "oro": 26, "plata": 44,  "bronce": 53, "npod": 191
+        "oro": 26, "plata": 44,  "bronce": 53, "npod": 192
     },{
         "posicion": 4, "nombre": "Jonel",
-        "oro": 22, "plata": 23, "bronce": 21, "npod": 223
+        "oro": 22, "plata": 23, "bronce": 21, "npod": 225
     },{
         "posicion": 6, "nombre": "galdi",
-        "oro": 21, "plata": 30, "bronce": 37, "npod": 177
+        "oro": 21, "plata": 30, "bronce": 37, "npod": 179
     },{
         "posicion": 5, "nombre": "berasa",
-        "oro": 22, "plata": 22, "bronce": 37, "npod": 188
+        "oro": 22, "plata": 22, "bronce": 37, "npod": 190
     },{
-        "posicion": 7, "nombre": "runspeltinskin",
-        "oro": 17, "plata": 19, "bronce": 37, "npod": 234
+        "posicion": 8, "nombre": "runspeltinskin",
+        "oro": 17, "plata": 19, "bronce": 37, "npod": 236
     },{
-        "posicion": 8, "nombre": "manaria",
-        "oro": 16, "plata": 24, "bronce": 25, "npod": 198
+        "posicion": 7, "nombre": "manaria",
+        "oro": 17, "plata": 24, "bronce": 25, "npod": 199
     },{
         "posicion": 9, "nombre": "JonBaraja",
         "oro": 16, "plata": 14, "bronce": 14, "npod": 57
@@ -31,55 +31,58 @@ var medalleroHistorico = [
         "oro": 8, "plata": 20, "bronce": 11, "npod": 74
     },{
         "posicion": 11, "nombre": "zabalaundi",
-        "oro": 3, "plata": 8, "bronce": 6, "npod": 51
+        "oro": 3, "plata": 9, "bronce": 6, "npod": 52
     },{
         "posicion": 12, "nombre": "baxxter",
-        "oro": 2, "plata": 1,  "bronce": 1, "npod": 26
+        "oro": 2, "plata": 2,  "bronce": 2, "npod": 27
     },{
         "posicion": 13, "nombre": "aberasa",
         "oro": 1, "plata": 2, "bronce": 0, "npod": 12
     },{
         "posicion": 14, "nombre": "ELORRI",
-        "oro": 1, "plata": 1, "bronce": 1, "npod": 12
+        "oro": 1, "plata": 1, "bronce": 1, "npod": 13
     },{
         "posicion": 15, "nombre": "castlefont",
         "oro": 0, "plata": 1, "bronce": 0, "npod": 0
     },{
         "posicion": 16, "nombre": "korroskada",
-        "oro": 0, "plata": 0, "bronce": 0, "npod": 2
+        "oro": 0, "plata": 0, "bronce": 0, "npod": 4
     }
 ];
 var medallero2024 = [
     {
         "posicion": 1, "nombre": "gurrutxaga",
-        "oro": 7, "plata": 1, "bronce": 1, "npod": 10
+        "oro": 7, "plata": 1, "bronce": 2, "npod": 10
     },{
         "posicion": 2, "nombre": "asiermaster",
-        "oro": 4, "plata": 5, "bronce": 5, "npod": 6
+        "oro": 5, "plata": 5, "bronce": 5, "npod": 7
     },{
-        "posicion": 3, "nombre": "Jonel",
-        "oro": 3, "plata": 1, "bronce": 0, "npod": 16
+        "posicion": 4, "nombre": "Jonel",
+        "oro": 3, "plata": 1, "bronce": 0, "npod": 18
     },{
-        "posicion": 4, "nombre": "runspeltinskin",
-        "oro": 2, "plata": 3, "bronce": 3, "npod": 12
+        "posicion": 5, "nombre": "runspeltinskin",
+        "oro": 2, "plata": 3, "bronce": 3, "npod": 14
     },{
-        "posicion": 5, "nombre": "manaria",
-        "oro": 2, "plata": 3, "bronce": 2, "npod": 13
+        "posicion": 3, "nombre": "manaria",
+        "oro": 3, "plata": 3, "bronce": 2, "npod": 14
     },{
         "posicion": 6, "nombre": "galdi",
-        "oro": 1, "plata": 3, "bronce": 0, "npod": 14
+        "oro": 1, "plata": 3, "bronce": 0, "npod": 16
     },{
         "posicion": 7, "nombre": "berasa",
-        "oro": 1, "plata": 1, "bronce": 2, "npod": 14
+        "oro": 1, "plata": 1, "bronce": 2, "npod": 16
     },{
         "posicion": 8, "nombre": "OIER19",
-        "oro": 0, "plata": 2,  "bronce": 7, "npod": 11
+        "oro": 0, "plata": 3,  "bronce": 7, "npod": 12
     },{
         "posicion": 9, "nombre": "baxxter",
-        "oro": 0, "plata": 1,  "bronce": 0, "npod": 3
+        "oro": 0, "plata": 1,  "bronce": 1, "npod": 4
     },{
         "posicion": 10, "nombre": "zabalaundi",
-        "oro": 0, "plata": 0, "bronce": 0, "npod": 3
+        "oro": 0, "plata": 1, "bronce": 0, "npod": 4
+    },{
+        "posicion": 11, "nombre": "korroskada",
+        "oro": 0, "plata": 0, "bronce": 0, "npod": 2
     }
 ];
 var medallero2023 = [
@@ -144,5 +147,23 @@ var medallero2022 = [
     },{
         "posicion": 10, "nombre": "ELORRI",
         "oro": 0, "plata": 0, "bronce": 0, "npod": 1
+    }
+];
+
+
+
+
+
+
+var medallero2007 = [
+    {
+        "posicion": 1, "nombre": "berasa",
+        "oro": 3, "plata": 0, "bronce": 0, "npod": 0
+    },{
+        "posicion": 2, "nombre": "manaria",
+        "oro": 0, "plata": 2, "bronce": 1, "npod": 0
+    },{
+        "posicion": 3, "nombre": "OIER19",
+        "oro": 0, "plata": 1, "bronce": 1, "npod": 0
     }
 ];
