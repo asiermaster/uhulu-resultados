9f8542f13a3e91696efda6765635a0c5014dba48
arodriguez <asier.rodriguez@onenpro.com>
2025-04-27 19:32:16 +0200

    Add medalleros in index2

--- js/index2.js
+++ js/index2.js
@@ -1,14 +1,17 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-    var $table = $('#tableHisEdi');
+    var $table = $('#tableMedallero');
     var $tablePuntos =  $('#tablePuntos');
     $(function () {
         $table.bootstrapTable({});
         $tablePuntos.bootstrapTable({});
       });
 
+    // Side bar menu events
     addEventSidebarButton();
     addEventCampeonatoDelMundo()
+    addEventMedalleroHistorico();
+    addEventMedalleroYear();
 
     goToDefaultPage()
 
@@ -41,8 +44,9 @@ function addEventCampeonatoDelMundo(){
     }
 }
 
+
 function loadCampeonatoDelMundo(sidebarMenu){
-    var $table = $('#tableHisEdi');
+    var $table = $('#tableMedallero');
     $table.bootstrapTable('load', historico_clasificaciones_year);
     $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
     var $tablePuntos =  $('#tablePuntos');
@@ -53,6 +57,56 @@ function loadCampeonatoDelMundo(sidebarMenu){
     $("#puntosTitle").html("Puntos histórico");
 
     $("#titlePagina").html(sidebarMenu.innerText);
+}
+
+function addEventMedalleroHistorico(){
+    const sidebarMealleroHist = document.body.querySelector('#medallero_historico');
+    if (sidebarMealleroHist) {
+        sidebarMealleroHist.addEventListener('click', event => {
+            event.preventDefault();
+            $(function () {
+                loadMedalleroHistorico();
+              })
+            
+        });
+    }
+}
+
+function loadMedalleroHistorico(){
+    var $table = $('#tableMedallero');
+    $table.bootstrapTable('load', medalleroHistorico);
+    $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    var $tablePuntos =  $('#tablePuntos');
+    $tablePuntos.bootstrapTable('load', puntos_totales);
+    $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+
+    $("#medalleroTitle").html("Medallero histórico");
+    $("#puntosTitle").html("Puntos histórico");
+}
+
+function addEventMedalleroYear(){
+    document.body.querySelectorAll("[id^='medallero_20']").forEach(function (sidebarMealleroYear) {
+        sidebarMealleroYear.addEventListener('click', event => {
+            event.preventDefault();
+            var idEvent = event.currentTarget.getAttribute('id');
+            $(function () {
+                loadMedalleroYear(idEvent.substr(idEvent.length - 4));
+              })
+            
+        });
+    });
+}
+
+function loadMedalleroYear(year){
+    var $table = $('#tableMedallero');
+    $table.bootstrapTable('load', window["medallero" + year]);
+    $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    var $tablePuntos =  $('#tablePuntos');
+    $tablePuntos.bootstrapTable('load', window["puntos" + year]);
+    $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    $("#medalleroTitle").html("Medallero " + year);
+    $("#puntosTitle").html("Puntos " + year);
+    window.scrollTo(0, 0);
 }
 
 
