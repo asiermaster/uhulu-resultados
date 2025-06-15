bab032010345229c2e9d6d5115bc32e1e10b6f62
arodriguez <asier.rodriguez@onenpro.com>
2025-06-15 19:01:41 +0200

    New index

--- js/index.js
+++ js/index.js
@@ -1,45 +1,125 @@
-/*!
-* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
-*/
-// 
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
-    var $table = $('#tableHisEdi');
-    var $tablePuntos =  $('#tablePuntos');
+    hideEveryRow();
 
-    $(function () {
-      $table.bootstrapTable({});
-      $tablePuntos.bootstrapTable({});
-    });
-    // Side bar events
-    addToggleSideBar();
+    initTables();
+
+    // Side bar menu events
+    addEventSidebarButton();
+    addEventCampeonatoDelMundo()
     addEventMedalleroHistorico();
     addEventMedalleroYear();
-    //Go to default
-    goToDefaultMedallero();
+    addEventMejorPeor();
+    addEventTop10();
+
+    goToDefaultPage();
+    
 
 });
 
+function initTables() {
+    var $table = $('#tableMedallero');
+    var $tablePuntos = $('#tablePuntos');
+    var $tableMejorPeor = $('#tableMejorPeor');
+    var $tableTop10 = $('#tableTop10');
 
-function addToggleSideBar(){
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
+
+    $table.bootstrapTable({});
+    $tablePuntos.bootstrapTable({});
+    $tableMejorPeor.bootstrapTable({});
+    $tableMejorPeor.bootstrapTable('load', ultimo_competicion_veces);
+    $tableMejorPeor.bootstrapTable('sortBy', { field: 'posicion', sortOrder: 'asc' });
+    $tableMejorPeor.find('tbody tr').each(function (index) {
+        // Apply gold color to the first row (index 0)
+        if (index === 0) {
+            $(this).addClass('bg-gold');
+        }
+
+        // Apply silver color to the second row (index 1)
+        else if (index === 1) {
+            $(this).addClass('bg-silver');
+        }
+
+        // Apply bronze color to the third row (index 2)
+        else if (index === 2) {
+            $(this).addClass('bg-bronze');
+        }
+    });
+    $tableTop10.bootstrapTable({});
+    $tableTop10.bootstrapTable('load', top10posicionestropela);
+    $tableTop10.bootstrapTable('sortBy', { field: 'posiciontropela', sortOrder: 'asc' });
+    $tableTop10.find('tbody tr').each(function (index) {
+        // Apply gold color to the first row (index 0)
+        if (index === 0) {
+            $(this).addClass('bg-gold');
+        }
+
+        // Apply silver color to the second row (index 1)
+        else if (index === 1) {
+            $(this).addClass('bg-silver');
+        }
+
+        // Apply bronze color to the third row (index 2)
+        else if (index === 2) {
+            $(this).addClass('bg-bronze');
+        }
+    });
+}
+
+function hideEveryRow(){
+    $('#rowMedalleros').hide();
+    $('#rowMejorPeor').hide();
+    $('#rowTop10').hide();
+}
+
+function showMedalleroRow(){
+    $('#rowMedalleros').show();
+}
+
+function addEventSidebarButton() {
+    const toggler = document.querySelector(".btn");
+    toggler.addEventListener("click", function () {
+        document.querySelector("#sidebar").classList.toggle("collapsed");
+    });
+    if (window.innerWidth <= 768) { // typical mobile threshold
+        toggler.click();
+      }
+}
+
+function goToDefaultPage(){
+    const sidebarMenu = document.body.querySelector('#campeonatos_mundo');
+    sidebarMenu.click();
+}
+
+function addEventCampeonatoDelMundo(){
+    const sidebarMenu = document.body.querySelector('#campeonatos_mundo');
+    if (sidebarMenu) {
+        sidebarMenu.addEventListener('click', event => {
             event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            $(function () {
+                hideEveryRow();
+                showMedalleroRow();
+                loadCampeonatoDelMundo(sidebarMenu);
+              })
+            
         });
     }
+}
+
+
+function loadCampeonatoDelMundo(sidebarMenu){
+    var $table = $('#tableMedallero');
+    $table.bootstrapTable('load', []);
+    $table.bootstrapTable('load', historico_clasificaciones_year);
+    $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    var $tablePuntos =  $('#tablePuntos');
+    $tablePuntos.bootstrapTable('load', []);
+    $tablePuntos.bootstrapTable('load', puntos_totales);
+    $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    
+    $("#medalleroTitle").html("Medallero campeonatos del mundo");
+    $("#puntosTitle").html("Puntos histórico");
+
 }
 
 function addEventMedalleroHistorico(){
@@ -48,11 +128,27 @@ function addEventMedalleroHistorico(){
         sidebarMealleroHist.addEventListener('click', event => {
             event.preventDefault();
             $(function () {
+                hideEveryRow();
+                showMedalleroRow();
                 loadMedalleroHistorico();
               })
             
         });
     }
+}
+
+function loadMedalleroHistorico(){
+    var $table = $('#tableMedallero');
+    $table.bootstrapTable('load', []);
+    $table.bootstrapTable('load', medalleroHistorico);
+    $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    var $tablePuntos =  $('#tablePuntos');
+    $tablePuntos.bootstrapTable('load', []);
+    $tablePuntos.bootstrapTable('load', puntos_totales);
+    $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+
+    $("#medalleroTitle").html("Medallero histórico");
+    $("#puntosTitle").html("Puntos histórico");
 }
 
 function addEventMedalleroYear(){
@@ -61,6 +157,8 @@ function addEventMedalleroYear(){
             event.preventDefault();
             var idEvent = event.currentTarget.getAttribute('id');
             $(function () {
+                hideEveryRow();
+                showMedalleroRow();
                 loadMedalleroYear(idEvent.substr(idEvent.length - 4));
               })
             
@@ -68,48 +166,48 @@ function addEventMedalleroYear(){
     });
 }
 
-function loadMedalleroHistorico(){
-    var $table = $('#tableHisEdi');
-    $table.bootstrapTable('load', medalleroHistorico);
-    $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
-    var $tablePuntos =  $('#tablePuntos');
-    $tablePuntos.bootstrapTable('load', puntos_totales);
-    $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
-
-    $("#medalleroTitle").html("Medallero histórico");
-    $("#puntosTitle").html("Puntos histórico");
-}
-
 function loadMedalleroYear(year){
-    var $table = $('#tableHisEdi');
+    var $table = $('#tableMedallero');
+    $table.bootstrapTable('load', []);
     $table.bootstrapTable('load', window["medallero" + year]);
     $table.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
     var $tablePuntos =  $('#tablePuntos');
-    $tablePuntos.bootstrapTable('load', window["puntos" + year]);
-    $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    $tablePuntos.bootstrapTable('load', []);
+    if ("puntos" + year in window) {
+        $tablePuntos.bootstrapTable('load', window["puntos" + year]);
+        $tablePuntos.bootstrapTable('sortBy', {field: 'posicion', sortOrder: 'asc'});
+    }
+    
     $("#medalleroTitle").html("Medallero " + year);
     $("#puntosTitle").html("Puntos " + year);
     window.scrollTo(0, 0);
 }
 
-function goToDefaultMedallero(){
-    const sidebarMealleroHist = document.body.querySelector('#medallero_historico');
-    sidebarMealleroHist.click();
+
+function addEventMejorPeor(){
+    const sidebarMenu = document.body.querySelector('#mejorpeor');
+    if (sidebarMenu) {
+        sidebarMenu.addEventListener('click', event => {
+            event.preventDefault();
+            $(function () {
+                hideEveryRow();
+                $('#rowMejorPeor').show();
+              })
+            
+        });
+    }
 }
 
-function puntosRowStyle(row, index) {
-    var classes = [
-      'bg-gold',
-      'bg-silver',
-      'bg-bronze'
-    ];
-
-    if (row.posicion <4) {
-      return {
-        classes: classes[row.posicion-1]
-      }
+function addEventTop10(){
+    const sidebarMenu = document.body.querySelector('#top10');
+    if (sidebarMenu) {
+        sidebarMenu.addEventListener('click', event => {
+            event.preventDefault();
+            $(function () {
+                hideEveryRow();
+                $('#rowTop10').show();
+              })
+            
+        });
     }
-    return {
-      
-    }
-  }
+}
